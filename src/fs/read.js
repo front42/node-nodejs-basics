5998aa23a69414f573b6f56074809e2b184c9838
front42 <altaifront@gmail.com>
2024-01-25 22:11:57 +0300

    feat: add fs read task solution

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,17 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const pathToReadFile = path.join(__dirname, 'files', 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+  try {
+    console.log(await fs.promises.readFile(pathToReadFile, 'utf-8'));
+  } catch (error) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await read();
