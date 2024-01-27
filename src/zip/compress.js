ebfe74bedc087edf03d1045e5414363742b790e3
front42 <altaifront@gmail.com>
2024-01-27 15:10:04 +0300

    feat: add zip compress task solution

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,21 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { createGzip } from 'zlib';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const pathToOriginalFile = path.join(__dirname, 'files', 'fileToCompress.txt');
+const pathToArchive = path.join(__dirname, 'files', 'archive.gz');
+
 const compress = async () => {
-    // Write your code here 
+  if (fs.existsSync(pathToOriginalFile)) {
+    let readableStream = fs.createReadStream(pathToOriginalFile);
+    let writableStream = fs.createWriteStream(pathToArchive);
+    readableStream.pipe(createGzip()).pipe(writableStream);
+  } else {
+    throw new Error('ZIP operation failed');
+  }
 };
 
 await compress();
