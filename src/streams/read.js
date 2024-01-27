99801468ca02539d53350759e79beb3b5d0a26f3
front42 <altaifront@gmail.com>
2024-01-27 23:41:42 +0300

    feat: add streams read task solution

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,15 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { stdout } from 'process';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const pathToReadFile = path.join(__dirname, 'files', 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+  let readableStream = fs.createReadStream(pathToReadFile);
+  readableStream.pipe(stdout);
 };
 
 await read();
