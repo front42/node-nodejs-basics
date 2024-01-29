c677d003462887dbf173cf93a3e0079c27c322f1
front42 <altaifront@gmail.com>
2024-01-29 07:32:09 +0300

    feat: add cli args task solution

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
-    // Write your code here 
+  let list = '';
+  const args = process.argv.slice(2);
+  for (let str of args) {
+    str.startsWith('--')
+      ? (list += list ? `, ${str.slice(2)}` : `${str.slice(2)}`)
+      : (list += ` is ${str}`);
+  }
+  list ? console.log(list) : console.log('No arguments');
 };
 
 parseArgs();
