6ac598ed9a5212047b2e8a980a954a135f8fa7c5
front42 <altaifront@gmail.com>
2024-01-30 00:45:06 +0300

    test: spawn child process with stdin/stdout in cp task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,24 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
+import path from 'path';
+import { fileURLToPath } from 'url';
+import child_process from 'child_process';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const pathToWorker = path.join(__dirname, 'files', 'script.js');
+
+const spawnChildProcess = async args => {
+  let worker = child_process.spawn('node', [pathToWorker], {
+    stdio: ['inherit', 'inherit', 'inherit', 'ipc'],
+  });
+
+  worker.on('error', error => {
+    console.log('Error:', error.message);
+  });
+
+  worker.on('close', code => {
+    console.log('Child process exited with code', code);
+  });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(/* [someArgument1, someArgument2, ...] */);
